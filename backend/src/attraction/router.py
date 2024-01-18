20c926fa2d78f5cbd58e07c02773c51d3504842d
Kwiatuh <46192203+kwiats@users.noreply.github.com>
2024-01-18 19:50:50 +0100

    #14 deployment to development server (#15)
    * #14-backend: added new dependencies to requirements and fix some mypy errors
    
    * #14-backend: added __init__.py file to folders
    
    * #14-backend: added deploy and check quality actions
    
    * #14-backend: added dockerfile
    
    * #14-backend: added configurations for ruff and mypy
    
    * #14-backend: added .gitignore
    
    * #14-backend: changed version of python to 3.11
    
    * #14-backend: downgrade version from 3.12 to 3.11 in backend.yml
    
    * #14-backend: delete all unnecessary comments
    
    * #14-backend: change version in docker
    
    * #14-backend: added docker-compose
    
    * #14-backend: updated dockerfile
    
    * #14-backend: change retrun for router in attraction
    
    * #14-backend: disabled empty-body error and none type return

--- backend/src/attraction/router.py
+++ backend/src/attraction/router.py
@@ -1,20 +1,22 @@
 from fastapi import APIRouter
 
+from src.attraction.schemas import Attraction
+
 router = APIRouter()
 
 
 @router.get("/all")
-async def get_all_attractions():
-    pass
+def get_all_attractions() -> list[Attraction]:
+    ...
 
 
 @router.get("/{id}")
-async def get_attraction_by_id():
-    pass
+def get_attraction_by_id() -> Attraction:
+    ...
 
 
 @router.get("/{latitude},{longitude}/")
-async def get_all_attractions_by_coords(latitude: float, longitude: float):
+async def get_all_attractions_by_coords(
+    latitude: float, longitude: float
+) -> dict[str, float]:
     return {"latitude": round(latitude, 4), "longitude": round(longitude, 4)}
-
-
--- backend/src/attraction/schemas.py
+++ backend/src/attraction/schemas.py
@@ -1,19 +1,21 @@
+from typing import List
+
 from pydantic import BaseModel
+from pydantic_extra_types.coordinate import Coordinate
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy.orm import declarative_base
-from pydantic_extra_types.coordinate import Coordinate
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class AttractionOrm(Base):
-    __tablename__ = 'attractions'
+    __tablename__ = "attractions"
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(63), unique=True)
     description = Column(String(255))
-    coords = Column(ARRAY(String(255), as_tuple=True))
+    coords: List[str] = Column(ARRAY(String(255), as_tuple=True))
 
 
 class Attraction(BaseModel):
