f9cf9653c78c04b4235ae966f02aee932bbf959a
kwiats <pawuskwiatkowski@gmail.com>
2024-01-20 16:49:08 +0100

    #23-backend: added register.py to register all middlewares, routers etc.

---
+++ backend/src/core/register.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+
+from src.attraction.routes.router import router as attraction_router
+from src.core.logger import LoggerSetup
+from src.database import Base, engine
+
+
+def init_app(version: str) -> FastAPI:
+    Base.metadata.create_all(bind=engine)
+    return FastAPI(version=version)
+
+
+def _read_version() -> str:
+    with open("src/VERSION.txt", "r") as version_file:
+        version = version_file.read()
+        return version
+
+
+def register_app():
+    VERSION = _read_version()
+    # config = config.Settings()
+    app = init_app(version=VERSION)
+
+    register_router(app)
+
+    register_logger()
+    return app
+
+
+def register_router(app: FastAPI):
+    app.include_router(attraction_router, prefix="/attractions")
+
+
+def register_logger():
+    logger = LoggerSetup()
