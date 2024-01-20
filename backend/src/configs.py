19134cc2fd5abe37ea15ee72dbced48cf052e526
kwiats <pawuskwiatkowski@gmail.com>
2024-01-20 16:59:31 +0100

    #23-backend: back to old version

--- backend/src/configs.py
+++ backend/src/configs.py
@@ -1,8 +1,7 @@
 import os
 
-
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-POSTGRES_DB_NAME = os.getenv("POSTGRES_DB", "postgres")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "jan_pawel_2")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "zajebal_mi")
+POSTGRES_DB_NAME = os.getenv("POSTGRES_DB", "szlugi")
