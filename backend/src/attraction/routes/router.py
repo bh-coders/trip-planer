3d4d233aa7b23f7af402a5c286c04e4000fd2279
kwiats <pawuskwiatkowski@gmail.com>
2024-01-21 01:18:19 +0100

    #23-backend: deleted logs

--- backend/src/attraction/routes/router.py
+++ backend/src/attraction/routes/router.py
@@ -16,21 +16,16 @@ _attraction_service = AttractionService(in_memory_repo)
 
 @router.get("/all")
 def get_all_attractions(db: Session = Depends(get_db)):
-    logger.info("Getting all attractions")
     return _attraction_service.get_all_attractions(db)
 
 
 @router.get("/{attraction_id}")
 def get_attraction_by_id(attraction_id: int, db: Session = Depends(get_db)):
-    logger.info(f"Getting attraction by ID: {attraction_id}")
-
     return _attraction_service.get_attraction_by_id(db, attraction_id)
 
 
 @router.post("/create")
 def create_attraction(attraction: AttractionSchema, db: Session = Depends(get_db)):
-    logger.info("Creating new attraction")
-
     return _attraction_service.create_attraction(db, attraction)
 
 
@@ -40,13 +35,9 @@ def update_attraction(
     updated_attraction: AttractionSchema,
     db: Session = Depends(get_db),
 ):
-    logger.info(f"Updating attraction with ID: {attraction_id}")
-
     return _attraction_service.update_attraction(db, attraction_id, updated_attraction)
 
 
 @router.delete("/{attraction_id}/delete")
 def delete_attraction(attraction_id: int, db: Session = Depends(get_db)):
-    logger.info(f"Deleting attraction with ID: {attraction_id}")
-
     return _attraction_service.delete_attraction(db, attraction_id)
