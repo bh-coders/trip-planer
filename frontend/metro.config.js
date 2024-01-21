356e8ae27e13515f1797a7b134db6a291ffdbdf3
MaciejKan <101216341+maciej-MKan@users.noreply.github.com>
2024-01-21 21:54:09 +0100

    43-frontend-remove_expo (#44)
    * 43-frontend-remove_expo
    
    * 43-fix_compiller_optons

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,10 +1,11 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-// eslint-disable-next-line no-undef
-const config = getDefaultConfig(__dirname, {
-  // Additional features...
-});
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
 
-module.exports = config;
-
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
