09d8e41b7e9f650dfb7b5452f92f09605f601793
kwiats <pawuskwiatkowski@gmail.com>
2024-01-21 16:15:05 +0100

    #38-documentation: deleted loki_instance_url variable

--- backend/src/core/configs.py
+++ backend/src/core/configs.py
@@ -5,5 +5,3 @@ POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 POSTGRES_DB_NAME = os.getenv("POSTGRES_DB", "postgres")
-
-LOKI_INSTANCE_URL = os.getenv("LOKI_INSTANCE_URL", "")
