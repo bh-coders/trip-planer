84f2b02ad29e7d069327d14b9bb31450dd2a6e1f
kwiats <pawuskwiatkowski@gmail.com>
2024-01-21 01:17:48 +0100

    #23-backend: added saving to file

--- backend/src/core/logger.py
+++ backend/src/core/logger.py
@@ -1,5 +1,8 @@
 import logging
 import logging.handlers
+import sys
+
+from pythonjsonlogger import jsonlogger
 
 
 class LoggerSetup:
@@ -8,19 +11,26 @@ class LoggerSetup:
         self.setup_logging()
 
     def setup_logging(self):
-        LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+        # JSON Formatter
+        json_formatter = jsonlogger.JsonFormatter(
+            '%(asctime)s %(process)s %(levelname)s %(name)s %(module)s %(funcName)s %(lineno)d %(message)s'
+        )
 
-        formatter = logging.Formatter(LOG_FORMAT)
+        # Console Handler
+        console_handler = logging.StreamHandler(sys.stderr)
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(json_formatter)
 
-        console = logging.StreamHandler()
-        console.setFormatter(formatter)
-
+        # File Handler with JSON formatter
         log_file = "logs/api-logs.log"
-        file = logging.handlers.TimedRotatingFileHandler(
+        file_handler = logging.handlers.TimedRotatingFileHandler(
             filename=log_file, when="midnight", backupCount=5
         )
-        file.setFormatter(formatter)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(json_formatter)
 
-        self.logger.addHandler(console)
-        self.logger.addHandler(file)
+        # Setting up the root logger
+        root_logger = logging.getLogger()
+        root_logger.setLevel(logging.DEBUG)
+        root_logger.addHandler(console_handler)
+        root_logger.addHandler(file_handler)
