0d4cf21c5b71a57999512ca1325df8b531ecc682
radthenone <krzyzak.codes@gmail.com>
2024-01-24 15:40:54 +0100

    #59-backend: add new models relationships

--- backend/src/attraction/models.py
+++ backend/src/attraction/models.py
@@ -1,13 +1,28 @@
+import uuid
+from typing import TYPE_CHECKING, Optional
+
+from sqlalchemy import JSON, UUID, Column, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, relationship
+
 from src.core.database import Base
-from sqlalchemy import Column, Integer, String, JSON, Float
+
+if TYPE_CHECKING:
+    from src.users.models import Profile
 
 
 class Attraction(Base):
     __tablename__ = "attractions"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    description = Column(String)
-    longitude = Column(Float)
-    latitude = Column(Float)
-    open_hours = Column(JSON, nullable=True)
+    id: int = Column(Integer, primary_key=True)
+    name: str = Column(String(255))
+    description: str = Column(String)
+    longitude: float = Column(Float)
+    latitude: float = Column(Float)
+    open_hours: Optional[dict] = Column(JSON, nullable=True)
+
+    profile_id: Optional[uuid.UUID] = Column(
+        UUID(as_uuid=True), ForeignKey("profiles.id"), nullable=True
+    )
+    profile: Mapped["Profile"] = relationship(
+        argument="Profile", back_populates="attractions"
+    )
