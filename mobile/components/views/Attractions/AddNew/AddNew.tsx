427fe588103985b303a3a405962f6e950021df15
maciej-MKan <kanabaj.maciej@gmail.com>
2024-01-25 00:42:56 +0100

    30-frontend-add_opening_hour_modal

--- mobile/components/views/Attractions/AddNew/AddNew.tsx
+++ mobile/components/views/Attractions/AddNew/AddNew.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 import StarRating from 'react-native-star-rating';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import OpeningHoursModal from './OpeningHour';
 import mapArrayToJson from '../tools/openingHourMapper';
 
@@ -29,30 +30,62 @@ const AddNewAttraction = () => {
     };
     return attractionData;
 =======
+=======
+import OpeningHoursModal from './OpeningHour';
+import mapArrayToJson from '../tools/openingHourMapper';
+>>>>>>> 6409910 (30-frontend-add_opening_hour_modal)
 
 const AddNewAttraction = () => {
+  const [name, setName] = useState('');
   const [coordinates, setCoordinates] = useState('');
   const [description, setDescription] = useState('');
   const [rating, setRating] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [openingHours, setOpeningHours] = useState();
 
+<<<<<<< HEAD
   const handleOpenHoursPress = () => {
     // Napisz logikę do przeniesienia do ekranu formularza godzin otwarcia
     console.log('Przejdź do formularza godzin otwarcia');
 >>>>>>> d7ffab7 (30-frontend-move_attractions_views_to_wo_expo)
+=======
+  const handleSaveOpeningHours = (hours) => {
+    setOpeningHours(hours);
+    setModalVisible(false);
+  };
+
+  const createAttractionJson = () => {
+    const attractionData = {
+      name: name,
+      description: description,
+      latitude: coordinates.substring(0, coordinates.indexOf(',')),
+      longitude: coordinates.substring(coordinates.indexOf(',') + 1),
+      open_hours: mapArrayToJson(openingHours),
+      rating: rating
+    };
+    return attractionData;
+>>>>>>> 6409910 (30-frontend-add_opening_hour_modal)
   };
 
   const handleAddAttractionPress = () => {
     // Napisz logikę do dodania atrakcji (np. wysłanie danych na serwer)
 <<<<<<< HEAD
+<<<<<<< HEAD
     console.log(createAttractionJson());
 =======
     console.log('Dodaj atrakcję', coordinates, description, rating);
 >>>>>>> d7ffab7 (30-frontend-move_attractions_views_to_wo_expo)
+=======
+    console.log(createAttractionJson());
+>>>>>>> 6409910 (30-frontend-add_opening_hour_modal)
   };
 
   return (
     <View style={{ padding: 16 }}>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 6409910 (30-frontend-add_opening_hour_modal)
       <Text>Name:</Text>
       <TextInput
         style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 16 }}
@@ -61,8 +94,11 @@ const AddNewAttraction = () => {
         onChangeText={(text) => setName(text)}
       />
 
+<<<<<<< HEAD
 =======
 >>>>>>> d7ffab7 (30-frontend-move_attractions_views_to_wo_expo)
+=======
+>>>>>>> 6409910 (30-frontend-add_opening_hour_modal)
       <Text>Coordinates:</Text>
       <TextInput
         style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 16 }}
@@ -81,12 +117,16 @@ const AddNewAttraction = () => {
       />
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 6409910 (30-frontend-add_opening_hour_modal)
       <Button title="Set Opening Hours" onPress={() => setModalVisible(true)} />
       <OpeningHoursModal
         initialOpeningHours={openingHours}
         visible={modalVisible}
         onClose={() => setModalVisible(false)}
         onSave={handleSaveOpeningHours}
+<<<<<<< HEAD
       />
 
       <View style={{ margin: 20 }}>
@@ -122,6 +162,27 @@ const AddNewAttraction = () => {
 
       <Button title="Add attraction" onPress={handleAddAttractionPress} />
 >>>>>>> d7ffab7 (30-frontend-move_attractions_views_to_wo_expo)
+=======
+      />
+
+      <View style={{ margin: 20 }}>
+        <Text style={{marginBottom:10}}>Attraction rate:</Text>
+        <StarRating
+          disabled={false}
+          maxStars={5}
+          emptyStar={'star-o'}
+          fullStar={'star'}
+          halfStar={'ios-star-half'}
+          rating={rating}
+          selectedStar={(rating) => setRating(rating)}
+          fullStarColor={'orange'}
+        />
+      </View>
+
+      <View style={{ padding: 20, borderRadius: 10, marginTop: 50, alignItems: 'flex-end' }}>
+        <Button title="Save" color={'green'} onPress={handleAddAttractionPress} />
+      </View>
+>>>>>>> 6409910 (30-frontend-add_opening_hour_modal)
     </View>
   );
 };
--- mobile/routers/components/CustomDrawnerContent.tsx
+++ mobile/routers/components/CustomDrawnerContent.tsx
@@ -3,33 +3,20 @@ import { DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navi
 import { View } from 'react-native';
 import AttractionSubMenu from '../../components/views/Attractions/Router';
 import TestSubMenu from '../../components/views/test/Router';
+import AttractionSubMenu from '../../components/views/Attractions/Router';
+import TestSubMenu from '../../components/views/test/Router';
 
 
 const CustomDrawerContent: React.FC<any> = (props) => {
-  const [nestedMenuOpen, setNestedMenuOpen] = useState(false);
+
 
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
       <AttractionSubMenu {...props} />
       <TestSubMenu {...props} />
-      <DrawerItem
-        label="Attractions"
-        onPress={() => setNestedMenuOpen(!nestedMenuOpen)}
-        focused={nestedMenuOpen}
-      />
-      {nestedMenuOpen && (
-        <View style={{ marginLeft: 16 }}>
-          <DrawerItem label="Search attraction" onPress={() => {
-            props.navigation.navigate('Search');
-            setNestedMenuOpen(false);
-          }} />
-          <DrawerItem label="Add new attrsction" onPress={() => {
-            props.navigation.navigate('AddNew');
-            setNestedMenuOpen(false);
-          }} />
-        </View>
-      )}
+      <AttractionSubMenu {...props} />
+      <TestSubMenu {...props} />
     </DrawerContentScrollView>
   );
 };
