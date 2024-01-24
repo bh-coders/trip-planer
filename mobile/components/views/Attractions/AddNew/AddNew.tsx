b8d7ea2ab0a0a9c8cbc119a8af63cf07818e8691
maciej-MKan <kanabaj.maciej@gmail.com>
2024-01-25 00:42:56 +0100

    30-frontend-add_opening_hour_modal

--- mobile/components/views/Attractions/AddNew/AddNew.tsx
+++ mobile/components/views/Attractions/AddNew/AddNew.tsx
@@ -1,24 +1,49 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 import StarRating from 'react-native-star-rating';
+import OpeningHoursModal from './OpeningHour';
+import mapArrayToJson from '../tools/openingHourMapper';
 
 const AddNewAttraction = () => {
+  const [name, setName] = useState('');
   const [coordinates, setCoordinates] = useState('');
   const [description, setDescription] = useState('');
   const [rating, setRating] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [openingHours, setOpeningHours] = useState();
 
-  const handleOpenHoursPress = () => {
-    // Napisz logikę do przeniesienia do ekranu formularza godzin otwarcia
-    console.log('Przejdź do formularza godzin otwarcia');
+  const handleSaveOpeningHours = (hours) => {
+    setOpeningHours(hours);
+    setModalVisible(false);
+  };
+
+  const createAttractionJson = () => {
+    const attractionData = {
+      name: name,
+      description: description,
+      latitude: coordinates.substring(0, coordinates.indexOf(',')),
+      longitude: coordinates.substring(coordinates.indexOf(',') + 1),
+      open_hours: mapArrayToJson(openingHours),
+      rating: rating
+    };
+    return attractionData;
   };
 
   const handleAddAttractionPress = () => {
     // Napisz logikę do dodania atrakcji (np. wysłanie danych na serwer)
-    console.log('Dodaj atrakcję', coordinates, description, rating);
+    console.log(createAttractionJson());
   };
 
   return (
     <View style={{ padding: 16 }}>
+      <Text>Name:</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 16 }}
+        placeholder="Type name"
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+
       <Text>Coordinates:</Text>
       <TextInput
         style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 16 }}
@@ -36,20 +61,31 @@ const AddNewAttraction = () => {
         multiline
       />
 
-      <TouchableOpacity onPress={handleOpenHoursPress}>
-        <Text style={{ color: 'blue', marginBottom: 16 }}>Formularz godzin otwarcia</Text>
-      </TouchableOpacity>
-
-      <Text>Attraction rate:</Text>
-      <StarRating
-        disabled={false}
-        maxStars={5}
-        rating={rating}
-        selectedStar={(rating) => setRating(rating)}
-        fullStarColor={'orange'}
+      <Button title="Set Opening Hours" onPress={() => setModalVisible(true)} />
+      <OpeningHoursModal
+        initialOpeningHours={openingHours}
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        onSave={handleSaveOpeningHours}
       />
 
-      <Button title="Add attraction" onPress={handleAddAttractionPress} />
+      <View style={{ margin: 20 }}>
+        <Text style={{marginBottom:10}}>Attraction rate:</Text>
+        <StarRating
+          disabled={false}
+          maxStars={5}
+          emptyStar={'star-o'}
+          fullStar={'star'}
+          halfStar={'ios-star-half'}
+          rating={rating}
+          selectedStar={(rating) => setRating(rating)}
+          fullStarColor={'orange'}
+        />
+      </View>
+
+      <View style={{ padding: 20, borderRadius: 10, marginTop: 50, alignItems: 'flex-end' }}>
+        <Button title="Save" color={'green'} onPress={handleAddAttractionPress} />
+      </View>
     </View>
   );
 };
