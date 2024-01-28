e48110bef943f423825d7aa60fde83817c807ca9
kwiats <pawuskwiatkowski@gmail.com>
2024-01-28 12:02:51 +0100

    #69 - backend: used linters on cloudstorage.py

--- backend/src/core/cloudstorage.py
+++ backend/src/core/cloudstorage.py
@@ -44,7 +44,9 @@ class CloudStorage:
             return client
         except Exception as e:
             logger.error("Failed to connect to MinIO server: %s" % e)
-            raise HTTPException(status_code=500, detail="Unable to connect to MinIO server: %s" % e)
+            raise HTTPException(
+                status_code=500, detail="Unable to connect to MinIO server: %s" % e
+            )
 
     def _check_connection(self, client: Minio):
         try:
