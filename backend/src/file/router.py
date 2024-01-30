d5a3075b8caf9ee281c7c455a0e00bb0c4c71ac9
kwiats <pawuskwiatkowski@gmail.com>
2024-01-30 21:50:23 +0100

    #79 - backend: change name from attraction_id to object_id

--- backend/src/file/router.py
+++ backend/src/file/router.py
@@ -6,13 +6,13 @@ from src.file.services import _load_file_from_cloud
 file_router = APIRouter()
 
 
-@file_router.get("/{bucket_name}/{attraction_id}/{filename}")
+@file_router.get("/{bucket_name}/{object_id}/{filename}")
 def open_file_from_cloud_storage(
         bucket_name: str,
-        attraction_id: int,
+        object_id: int,
         filename: str,
         is_token_valid: bool = Depends(verify_jwt)):
-    return _load_file_from_cloud(filename, bucket_name, attraction_id)
+    return _load_file_from_cloud(filename, bucket_name, object_id)
 
 
 @file_router.get("/{bucket_name}/{filename}")
