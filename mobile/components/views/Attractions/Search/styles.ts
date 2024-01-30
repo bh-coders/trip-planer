5ed808d484324e3e999bfe3e51ccfbf4550488aa
maciej-MKan <kanabaj.maciej@gmail.com>
2024-01-31 00:38:02 +0100

    30-frontend-add_reactive_filters

--- mobile/components/views/Attractions/Search/styles.ts
+++ mobile/components/views/Attractions/Search/styles.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StyleSheet } from 'react-native';
 
 export const attractionSerchStyles = StyleSheet.create({
@@ -6,16 +5,6 @@ export const attractionSerchStyles = StyleSheet.create({
     width: '100%',
     padding: 10,
     marginVertical: 1
-=======
-import { StyleSheet, Dimensions } from 'react-native';
-
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
->>>>>>> d7ffab7 (30-frontend-move_attractions_views_to_wo_expo)
   },
   errorText: {
     fontSize: 24,
