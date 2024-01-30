681fc3ac19af7c386c971d7383badf64a9c2e2da
kwiats <pawuskwiatkowski@gmail.com>
2024-01-30 22:41:08 +0100

    #82 - hotfix/backend - reformat files

--- backend/src/file/router.py
+++ backend/src/file/router.py
@@ -8,16 +8,16 @@ file_router = APIRouter()
 
 @file_router.get("/{bucket_name}/{object_id}/{filename}")
 def open_file_from_cloud_storage(
-        bucket_name: str,
-        object_id: int,
-        filename: str,
-        is_token_valid: bool = Depends(verify_jwt)):
+    bucket_name: str,
+    object_id: int,
+    filename: str,
+    is_token_valid: bool = Depends(verify_jwt),
+):
     return _load_file_from_cloud(filename, bucket_name, object_id)
 
 
 @file_router.get("/{bucket_name}/{filename}")
 def open_default_file_from_cloud_storage(
-        bucket_name: str,
-        filename: str,
-        is_token_valid: bool = Depends(verify_jwt)):
+    bucket_name: str, filename: str, is_token_valid: bool = Depends(verify_jwt)
+):
     return _load_file_from_cloud(filename, bucket_name)
