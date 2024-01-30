e025a7e3f167b4fab28eb6ab1544a2c69fad898b
kwiats <pawuskwiatkowski@gmail.com>
2024-01-30 21:47:31 +0100

    #79 - backend: create service for read all images in attractions

--- backend/src/attraction/services/attraction_service.py
+++ backend/src/attraction/services/attraction_service.py
@@ -1,9 +1,12 @@
-from uuid import uuid4
+import logging
+
 from sqlalchemy.orm import Session
-from src.attraction.schemas import AttractionSchema
+
 from src.attraction.interfaces.repository import Repository
+from src.attraction.schemas import AttractionSchema, AttractionsImages
+from src.core.cloudstorage import CloudStorage
 
-
+logger = logging.getLogger(__name__)
 class AttractionService:
     def __init__(self, repository: Repository):
         self._repository = repository
@@ -18,7 +21,7 @@ class AttractionService:
         return self._repository.create(db, attraction)
 
     def update_attraction(
-        self, db: Session, attraction_id: int, updated_attraction: AttractionSchema
+            self, db: Session, attraction_id: int, updated_attraction: AttractionSchema
     ):
         attraction = self.get_attraction_by_id(db, attraction_id)
         if attraction is None:
@@ -30,3 +33,29 @@ class AttractionService:
         if attraction is None:
             return False
         return self._repository.delete(db, attraction)
+
+    def get_attraction_images(self, db: Session, attraction_id: int) -> AttractionsImages:
+        cloud_service = CloudStorage()
+        attraction = self.get_attraction_by_id(db, attraction_id)
+
+        if attraction is None:
+            return AttractionsImages(id=None)
+
+        bucket_name = 'attractions'
+        default_path = f"{bucket_name}/default.jpg"
+        prefix_bucket = f"{attraction.id}/"
+
+        try:
+            objects = cloud_service.list_bucket_objects(bucket_name, prefix_bucket)
+        except Exception as e:
+            logger.error("Error retrieving objects from cloud server: %s " % e)
+            return AttractionsImages(id=attraction.id, image_urls=[default_path])
+
+        result = AttractionsImages(id=attraction.id, image_urls=[default_path])
+
+        for obj in objects:
+            if default_path in result.image_urls:
+                result.image_urls.remove(default_path)
+            result.image_urls.append(f"{bucket_name}/{obj.object_name}")
+
+        return result
