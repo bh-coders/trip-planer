3b5f91473bfa692785ffa726edd8498a8e2e17ac
kwiats <pawuskwiatkowski@gmail.com>
2024-02-02 19:53:07 +0100

    #62-backend: add property column to attraction

--- backend/src/attraction/models.py
+++ backend/src/attraction/models.py
@@ -1,24 +1,35 @@
 from typing import Optional
 
-from sqlalchemy import JSON, UUID, Column, Float, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    JSON,
+    UUID,
+    Column,
+    Float,
+    ForeignKey,
+    Integer,
+    String,
+    func,
+    select,
+)
+from sqlalchemy.orm import Mapped, column_property, relationship
 
 from src.core.database import Base
+from src.threads.models import Review
 
 
 class Attraction(Base):
     __tablename__ = "attractions"
 
-    id: int = Column(Integer, primary_key=True)
-    name: str = Column(String(255))
-    description: str = Column(String)
-    longitude: float = Column(Float)
-    latitude: float = Column(Float)
-    open_hours: Optional[dict] = Column(JSON, nullable=True)
-    address: str = Column(String)
-    city: str = Column(String)
-    country: str = Column(String)
-    category: str = Column(String)
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    name: Mapped[str] = Column(String(255))
+    description: Mapped[str] = Column(String)
+    longitude: Mapped[float] = Column(Float)
+    latitude: Mapped[float] = Column(Float)
+    open_hours: Mapped[Optional[dict]] = Column(JSON, nullable=True)
+    address: Mapped[str] = Column(String)
+    city: Mapped[str] = Column(String)
+    country: Mapped[str] = Column(String)
+    category: Mapped[str] = Column(String)
 
     # Many-to-one relationship with User
     user_id = Column(
@@ -27,3 +38,24 @@ class Attraction(Base):
         nullable=False,
     )
     user = relationship("User", back_populates="attractions")
+
+    average_rating: Mapped[float] = column_property(
+        select(func.coalesce(func.avg(Review.rating), 0))
+        .where(Review.attraction_id == id)
+        .correlate_except(Review)
+        .scalar_subquery()
+    )
+
+    average_time_spent: Mapped[float] = column_property(
+        select(func.coalesce(func.avg(Review.time_spent), 0))
+        .where(Review.attraction_id == id)
+        .correlate_except(Review)
+        .scalar_subquery()
+    )
+
+    average_price: Mapped[float] = column_property(
+        select(func.coalesce(func.avg(Review.price), 0))
+        .where(Review.attraction_id == id)
+        .correlate_except(Review)
+        .scalar_subquery()
+    )
