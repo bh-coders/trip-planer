cd97bbe514ddecaa8fcdabf4944b494cebf01720
radthenone <krzyzak.codes@gmail.com>
2024-02-03 00:07:02 +0100

    24-backend-imp-crud-feature-create-profile: change some global exceptions and correct verify_jwt exceptions, get_current_user exceptions and change return

--- backend/src/core/interceptors/auth_interceptor.py
+++ backend/src/core/interceptors/auth_interceptor.py
@@ -1,15 +1,20 @@
 import logging
-from typing import Annotated
+from typing import Annotated, Optional
 
 import jwt
-from fastapi import Depends, HTTPException
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from starlette import status
 
+from src.auth.exceptions import (
+    InvalidCredentials,
+    InvalidToken,
+    TokenExpired,
+    UserDoesNotExist,
+)
+from src.auth.models import User
 from src.auth.repositories.user_repo import UserRepository
 from src.auth.utils import decode_jwt_token
-from src.core.configs import ALGORITHM, SECRET_KEY
 from src.core.database import get_db
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -26,30 +31,27 @@ def verify_jwt(
         logger.info(payload)
         return True
     except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=403, detail="Token expired")
+        raise TokenExpired
     except jwt.InvalidTokenError:
-        raise HTTPException(status_code=403, detail="Invalid token")
+        raise InvalidToken
     except jwt.PyJWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
-        )
+        raise InvalidCredentials
 
 
 def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)],
     db: Annotated[Session, Depends(get_db)],
-) -> bool:
+) -> Optional[User]:
     try:
         payload = decode_jwt_token(token=token)
         logger.info(payload)
         user_id = payload.get("user_id")
-        if auth_repository.get_by_id(user_id=user_id, db=db):
-            return True
+        if user := auth_repository.get_by_id(user_id=user_id, db=db):
+            return user
+        raise UserDoesNotExist
     except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=403, detail="Token expired")
+        raise TokenExpired
     except jwt.InvalidTokenError:
-        raise HTTPException(status_code=403, detail="Invalid token")
+        raise InvalidToken
     except jwt.PyJWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
-        )
+        raise InvalidCredentials
