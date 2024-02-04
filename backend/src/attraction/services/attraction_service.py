91e71a8971ad56960140fad61e23f148acb13def
kwiats <pawuskwiatkowski@gmail.com>
2024-02-04 13:14:01 +0100

    #64-backend: added cache_storage to attractions

--- backend/src/attraction/services/attraction_service.py
+++ backend/src/attraction/services/attraction_service.py
@@ -1,49 +1,82 @@
+import json
 import logging
 
+from fastapi import HTTPException, Response
 from sqlalchemy.orm import Session
 
 from src.attraction.interfaces.repository import Repository
-from src.attraction.schemas import AttractionSchema, AttractionImages
-from src.core.cloudstorage import CloudStorage
+from src.attraction.schemas import AttractionImages, AttractionSchema
+from src.core.json_encoder import JSONEncoder
+from src.db.cache_storage import CacheStorage
+from src.db.cloudstorage import CloudStorage
 
 logger = logging.getLogger(__name__)
+cache = CacheStorage()
+cloud_storage = CloudStorage()
 
 
 class AttractionService:
     def __init__(self, repository: Repository):
         self._repository = repository
 
-    def get_all_attractions(self, db: Session):
-        return self._repository.get_all(db)
+    def get_all_attractions(self, db: Session) -> list[AttractionSchema]:
+        attractions = self._repository.get_all(db)
+        return [AttractionSchema(**attraction.as_dict()) for attraction in attractions]
 
-    def get_attraction_by_id(self, db: Session, attraction_id: int):
-        return self._repository.get_by_id(db, attraction_id)
+    def get_attraction_by_id(self, db: Session, attraction_id: int) -> AttractionSchema:
+        cache_attraction = cache.get_value(f"attraction:{attraction_id}")
+        if cache_attraction:
+            attraction = json.loads(cache_attraction)
+            return AttractionSchema(**attraction)
+        attraction = self._repository.get_by_id(db, attraction_id)
+
+        if not attraction:
+            raise HTTPException(status_code=404, detail="Attraction does not exist")
+        cache.set_value(
+            f"attraction:{attraction_id}",
+            json.dumps(attraction.as_dict(), cls=JSONEncoder),
+        )
+        return AttractionSchema(**attraction.as_dict())
 
     def create_attraction(self, db: Session, attraction: AttractionSchema):
-        return self._repository.create(db, attraction)
+        if not self._repository.create(db, attraction):
+            raise HTTPException(
+                status_code=400, detail="Attraction could not be created."
+            )
+        return Response(status_code=201, content="Attraction successfully created.")
 
     def update_attraction(
         self, db: Session, attraction_id: int, updated_attraction: AttractionSchema
-    ):
-        attraction = self.get_attraction_by_id(db, attraction_id)
+    ) -> AttractionSchema:
+        attraction = self._repository.get_by_id(db, attraction_id)
         if attraction is None:
-            return False
-        return self._repository.update(db, attraction, updated_attraction)
+            raise HTTPException(status_code=404, detail="Cannot found attraction.")
+        updated = self._repository.update(db, attraction, updated_attraction)
+        if not updated:
+            raise HTTPException(status_code=402, detail="Cannot update attraction.")
+        cache.set_value(f"attraction:{attraction_id}", updated.as_dict())
+        return AttractionSchema(**updated.as_dict())
 
     def delete_attraction(self, db: Session, attraction_id: int):
-        attraction = self.get_attraction_by_id(db, attraction_id)
+        attraction = self._repository.get_by_id(db, attraction_id)
         if attraction is None:
             return False
-        return self._repository.delete(db, attraction)
+        cache.delete_value(f"attraction:{attraction_id}")
+        if not self._repository.delete(db, attraction):
+            raise HTTPException(
+                status_code=400, detail="Attraction could not be deleted."
+            )
+        return Response(status_code=200, content="Attraction successfully deleted.")
 
     def get_attraction_images(
         self, db: Session, attraction_id: int
     ) -> AttractionImages:
-        cloud_storage = CloudStorage()
-        attraction = self.get_attraction_by_id(db, attraction_id)
+        cache_attraction_images = cache.get_value(f"attraction_images:{attraction_id}")
+        if cache_attraction_images:
+            attraction_images = json.loads(cache_attraction_images)
+            return AttractionImages(**attraction_images)
 
-        if attraction is None:
-            return AttractionImages(id=None)
+        attraction = self.get_attraction_by_id(db, attraction_id)
 
         bucket_name = "attractions"
         default_path = f"{bucket_name}/default.jpg"
@@ -61,5 +94,8 @@ class AttractionService:
             if default_path in result.image_urls:
                 result.image_urls.remove(default_path)
             result.image_urls.append(f"{bucket_name}/{obj.object_name}")
-
+        cache.set_value(
+            f"attraction_images:{attraction_id}",
+            json.dumps(result.model_dump(), cls=JSONEncoder),
+        )
         return result
