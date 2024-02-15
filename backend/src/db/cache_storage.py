66a68e1c08bfc283d168707199238c7d6001f834
radthenone <krzyzak.codes@gmail.com>
2024-02-15 18:40:47 +0100

    24-backend-imp-24-backend-imp-crud-for-users-model: add redis cache pub/sub for models

--- backend/src/db/cache_storage.py
+++ backend/src/db/cache_storage.py
@@ -1,6 +1,9 @@
+import json
 import logging
+import time
 from enum import Enum
 from typing import Optional
+from uuid import UUID
 
 from redis import Redis
 
@@ -10,6 +13,7 @@ from src.core.configs import (
     CACHE_STORAGE_PASSWORD,
     CACHE_STORAGE_PORT,
 )
+from src.db.database import Base
 
 logger = logging.getLogger(__name__)
 
@@ -33,6 +37,11 @@ class CacheStorage:
         self._port = port or CACHE_STORAGE_PORT
         self._password = password or CACHE_STORAGE_PASSWORD
         self.cache = self._connect_to_data_store()
+        self.pubsub = self.cache.pubsub()
+        self.client = self.RedisPubSub(
+            redis=self.cache,
+            client=self.pubsub,
+        )
 
         self.expiration_time: int = expiration or CACHE_STORAGE_EXP
 
@@ -63,3 +72,120 @@ class CacheStorage:
 
     def delete_value(self, key):
         self.cache.delete(key)
+
+    class RedisPubSub:
+        def __init__(self, redis, client):
+            self.redis = redis
+            self.client = client
+
+        def publish(
+            self,
+            pattern: str,
+            data: Optional[int | str | dict | float] = None,
+            suffix: Optional[str] = None,
+        ):
+            try:
+                pattern = f"{pattern}:{suffix}" if suffix else pattern
+                if isinstance(data, dict):
+                    data = json.dumps(data)
+                self.redis.publish(pattern, data)
+                logger.info(f"Published signal: {data} to pattern: {pattern}")
+                return True
+            except Exception as e:
+                logger.error("Could not publish signal: %s" % e)
+                return False
+
+        def subscribe(
+            self,
+            channel: str,
+            suffix: Optional[str] = None,
+        ):
+            full_channel = f"{channel}:{suffix}" if suffix else channel
+            self.client.subscribe(full_channel)
+            logger.info("Subscribed to channel: %s", full_channel)
+
+        def unsubscribe(self, channel: str, suffix: Optional[str] = None):
+            full_channel = f"{channel}:{suffix}" if suffix else channel
+            self.client.unsubscribe(full_channel)
+            logger.info("Unsubscribed from channel: %s", full_channel)
+
+        def message(self):
+            message = self.client.get_message()
+            if message is not None:
+                message_type = message["type"]
+                if message_type == "message":
+                    message_data = message["data"]
+                    if isinstance(
+                        message_data,
+                        str,
+                    ):
+                        try:
+                            message_data = json.loads(message_data)
+                        except json.decoder.JSONDecodeError:
+                            pass
+
+                    logger.info("Received message: %s", message_data)
+                    return message_data
+
+                elif message_type == "subscribe":
+                    if message.get("pattern") is not None:
+                        logger.info("Subscribed to pattern: %s", message["pattern"])
+                    else:
+                        logger.info("Subscribed")
+                elif message_type == "unsubscribe":
+                    logger.info("Unsubscribed from pattern: %s", message["pattern"])
+                else:
+                    logger.warning(
+                        "Received unhandled message type: %s. Message data: %s",
+                        message_type,
+                        message.get("data"),
+                    )
+            return None
+
+
+class CacheModel:
+    cache = CacheStorage()
+
+    def add(
+        self,
+        model: Base,
+        data: dict | str | int | float,
+        model_id: Optional[int | str | UUID] = None,
+    ):
+        if model_id is not None:
+            model_id = str(model_id)
+        self.cache.client.publish(
+            pattern=model.__tablename__,
+            suffix=model_id,
+            data=data,
+        )
+
+    def connect(
+        self,
+        model: Base,
+        model_id: Optional[int | str | UUID] = None,
+        many: Optional[bool] = False,
+    ):
+        if model_id is not None:
+            model_id = str(model_id)
+        suffix = model_id
+        if many:
+            suffix = "*"
+        self.cache.client.subscribe(
+            channel=model.__tablename__,
+            suffix=suffix,
+        )
+
+    def disconnect(
+        self,
+        model: Base,
+        model_id: Optional[int | str | UUID] = None,
+    ):
+        self.cache.client.unsubscribe(model.__tablename__, model_id)
+
+    def get(self):
+        while True:
+            message = self.cache.client.message()
+            if message:
+                return message
+            time.sleep(0.1)
--- backend/src/users/utils.py
+++ backend/src/users/utils.py
@@ -12,7 +12,7 @@ def prepare_profile_image(
     with urllib.request.urlopen(image_url) as response:
         contents = response.read()
         file = io.BytesIO(contents)
-        filename = f"{user_id}/{image_url.split('/')[-1]}"
+        filename = f"{user_id}/avatar.{image_url.split('.')[-1]}"
         file_size = len(contents)
         content_type = guess_type(image_url)[0] or "application/octet-stream"
     return {
