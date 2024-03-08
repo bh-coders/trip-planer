5b5e0435a534cfc772aec749b15b0f0259759bff
radthenone <krzyzak.codes@gmail.com>
2024-03-08 23:18:13 +0100

    113-new-auth-interceptor-for-token-and-user_id: fix review and comment routes and services with new interceptors

--- backend/src/threads/routes/router.py
+++ backend/src/threads/routes/router.py
@@ -4,7 +4,7 @@ from typing import Annotated, Optional
 from fastapi import APIRouter, Depends, Request, Response
 from sqlalchemy.orm import Session
 
-from src.core.interceptors.auth_interceptor import verify_jwt
+from src.core.interceptors.auth_interceptor import get_user_id, verify_jwt
 from src.db.cache_storage import RedisStorage
 from src.db.database import get_db
 from src.threads.repository.comment_repository import CommentRepository
@@ -30,47 +30,46 @@ review_service = ReviewService(repository=review_repository, cache=cache_storage
 
 @threads_router.post("/create")
 def create_thread(
-        thread: ReviewCreate,
-        request: Request,
-        db: Annotated[Session, Depends(get_db)],
-        is_token_valid: bool = Depends(verify_jwt),
+    thread: ReviewCreate,
+    user_id: Annotated[uuid.UUID, Depends(get_user_id)],
+    db: Annotated[Session, Depends(get_db)],
 ) -> Response:
-    return review_service.create(request=request, thread=thread, db=db)
+    return review_service.create(user_id=user_id, thread=thread, db=db)
 
 
 @threads_router.get("/{review_id}")
 def get_thread(
-        review_id: uuid.UUID,
-        db: Annotated[Session, Depends(get_db)],
-        is_token_valid: bool = Depends(verify_jwt),
+    review_id: uuid.UUID,
+    db: Annotated[Session, Depends(get_db)],
+    is_token_valid: bool = Depends(verify_jwt),
 ) -> ReviewSchema:
     return review_service.get_thread_by_id(thread_id=review_id, db=db)
 
 
 @threads_router.delete("/{review_id}")
 def delete_thread(
-        review_id: uuid.UUID,
-        db: Annotated[Session, Depends(get_db)],
-        is_token_valid: bool = Depends(verify_jwt),
+    review_id: uuid.UUID,
+    db: Annotated[Session, Depends(get_db)],
+    is_token_valid: bool = Depends(verify_jwt),
 ) -> Response:
     return review_service.delete_thread(db=db, review_id=review_id)
 
 
 @threads_router.delete("/comment/{comment_id}")
 def delete_comment(
-        comment_id: uuid.UUID,
-        db: Annotated[Session, Depends(get_db)],
-        is_token_valid: bool = Depends(verify_jwt),
+    comment_id: uuid.UUID,
+    db: Annotated[Session, Depends(get_db)],
+    is_token_valid: bool = Depends(verify_jwt),
 ) -> Response:
     return comment_service.delete_comment(db=db, comment_id=comment_id)
 
 
 @threads_router.patch("/{review_id}")
 def update_thread(
-        review_id: uuid.UUID,
-        review: ReviewUpdate,
-        db: Annotated[Session, Depends(get_db)],
-        is_token_valid: bool = Depends(verify_jwt),
+    review_id: uuid.UUID,
+    review: ReviewUpdate,
+    db: Annotated[Session, Depends(get_db)],
+    is_token_valid: bool = Depends(verify_jwt),
 ) -> ReviewSchema:
     return review_service.update_thread(
         db=db, review_id=review_id, updated_review=review
@@ -79,10 +78,10 @@ def update_thread(
 
 @threads_router.patch("/comment/{comment_id}")
 def update_comment(
-        comment_id: uuid.UUID,
-        comment: CommentUpdate,
-        db: Annotated[Session, Depends(get_db)],
-        is_token_valid: bool = Depends(verify_jwt),
+    comment_id: uuid.UUID,
+    comment: CommentUpdate,
+    db: Annotated[Session, Depends(get_db)],
+    is_token_valid: bool = Depends(verify_jwt),
 ) -> CommentSchema:
     return comment_service.update_comment(
         db=db, comment_id=comment_id, updated_comment=comment
@@ -91,13 +90,13 @@ def update_comment(
 
 @threads_router.get("/attraction/{attraction_id}")
 def get_threads_attraction(
-        db: Annotated[Session, Depends(get_db)],
-        attraction_id: int | str,
-        sort_by: Optional[str] = None,  # 'rating', 'price', 'time_spent'
-        rating: Optional[int] = None,
-        price: Optional[int] = None,
-        time_spent: Optional[int] = None,
-        is_token_valid: bool = Depends(verify_jwt),
+    db: Annotated[Session, Depends(get_db)],
+    attraction_id: int | str,
+    sort_by: Optional[str] = None,  # 'rating', 'price', 'time_spent'
+    rating: Optional[int] = None,
+    price: Optional[int] = None,
+    time_spent: Optional[int] = None,
+    is_token_valid: bool = Depends(verify_jwt),
 ) -> list[ReviewSchema]:
     return review_service.get_threads_attraction_filtered_sorted(
         db=db,
@@ -111,9 +110,8 @@ def get_threads_attraction(
 
 @threads_router.post("/comment")
 def comment_thread(
-        request: Request,
-        comment: CommentCreate,
-        db: Annotated[Session, Depends(get_db)],
-        is_token_valid: bool = Depends(verify_jwt),
+    comment: CommentCreate,
+    user_id: Annotated[uuid.UUID, Depends(get_user_id)],
+    db: Annotated[Session, Depends(get_db)],
 ) -> Response:
-    return comment_service.comment_thread(request=request, comment=comment, db=db)
+    return comment_service.comment_thread(user_id=user_id, comment=comment, db=db)
